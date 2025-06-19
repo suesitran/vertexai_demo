23c05dfbc034974d08806956e311cebdb9931fe0
suesitran <tranhathuy@gmail.com>
2025-06-20 09:12:45 +1000

    Prepare to add more generative media

---
+++ lib/screens/generative_chat.dart
@@ -0,0 +1,184 @@
+import 'dart:io';
+
+import 'package:firebase_ai/firebase_ai.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_core/flutter_chat_core.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../functions.dart';
+
+class GenerativeChat extends StatefulWidget {
+  const GenerativeChat({super.key});
+
+  @override
+  State<GenerativeChat> createState() => _GenerativeChatState();
+}
+
+class _GenerativeChatState extends State<GenerativeChat> {
+  final InMemoryChatController chatController = InMemoryChatController();
+  final FunctionsHandler functionsHandler = FunctionsHandler();
+
+  late final ChatSession chatSession =
+  FirebaseAI.vertexAI()
+      .generativeModel(
+    model: 'gemini-2.0-flash',
+    tools: [functionsHandler.functions],
+    systemInstruction: Content.system(
+      'You are Ducky, a smart chatbot that can help user with anything he needs.',
+    ),
+  )
+      .startChat();
+
+  final ValueNotifier<XFile?> attachment = ValueNotifier(null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Chat(
+      currentUserId: 'user',
+      resolveUser: (id) => Future.value(User(id: id, name: 'User')),
+      chatController: chatController,
+      onMessageSend: (text) {
+        // send message to Gemini
+        chatController.insertMessage(
+          Message.text(
+            id: '${chatController.messages.length}',
+            authorId: 'user',
+            text: text,
+            sentAt: DateTime.now(),
+          ),
+        );
+
+        _sendToGemini(text);
+      },
+      onAttachmentTap: () {
+        // add attachment to chat message
+        ImagePicker().pickImage(source: ImageSource.gallery).then((value) {
+          if (value != null) {
+            attachment.value = value;
+          }
+        });
+      },
+      builders: Builders(
+        imageMessageBuilder: (context, image, index) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child:
+            kIsWeb
+            // Image does not support File on Web platform,
+            // therefore we use Image.network for web platform
+                ? Image.network(image.source, width: 300)
+                : Image.file(File(image.source), width: 300),
+          );
+        },
+        composerBuilder: (p0) {
+          return Composer(
+            attachmentIcon: ValueListenableBuilder<XFile?>(
+              valueListenable: attachment,
+              builder: (context, value, child) {
+                final size = 30.0;
+                if (value == null) {
+                  return Icon(Icons.attachment, size: size);
+                }
+
+                return kIsWeb
+                // Image does not support File on Web platform,
+                // therefore we use Image.network for web platform
+                    ? Image.network(value.path, height: size)
+                    : Image.file(File(value.path), height: size);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> _sendToGemini(
+      String text, {
+        List<FunctionResponse>? functionResponses,
+        Message? oldMessage,
+        String response = '',
+      }) async {
+    if (text.isEmpty && attachment.value == null) {
+      // nothing to send
+      return;
+    }
+
+    if (attachment.value != null) {
+      chatController.insertMessage(
+        Message.image(
+          id: '${chatController.messages.length}',
+          authorId: 'user',
+          source: attachment.value!.path,
+        ),
+      );
+    }
+
+    oldMessage ??= Message.text(
+      id: '${chatController.messages.length}',
+      authorId: 'model',
+      text: response,
+    );
+    // only create empty message if functionResponse is null
+    if (functionResponses == null || functionResponses.isEmpty) {
+      chatController.insertMessage(oldMessage);
+    }
+
+    final Content content = Content.multi([
+      if (text.isNotEmpty) TextPart(text),
+      if (attachment.value != null)
+        InlineDataPart('image/*', await attachment.value!.readAsBytes()),
+      if (functionResponses != null && functionResponses.isNotEmpty)
+        ...functionResponses,
+    ]);
+
+    chatSession.sendMessageStream(content).listen((event) async {
+      if (event.functionCalls.isNotEmpty) {
+        List<FunctionResponse> functionResponses = await functionsHandler
+            .handleFunctionCalls(event.functionCalls, (value) {
+          // on file created
+          if (value.type == FileType.picture) {
+            final newMessage = Message.image(
+              id: oldMessage!.id,
+              authorId: oldMessage!.authorId,
+              text: response,
+              sentAt: DateTime.now(),
+              source: value.path,
+            );
+            chatController.updateMessage(oldMessage!, newMessage);
+
+            oldMessage = Message.text(
+              id: '${chatController.messages.length}',
+              authorId: 'model',
+              text: '',
+            );
+            chatController.insertMessage(oldMessage!);
+          }
+        });
+
+        _sendToGemini(
+          text,
+          functionResponses: functionResponses,
+          oldMessage: oldMessage,
+          response: response,
+        );
+        return;
+      }
+
+      response = '$response ${event.text ?? ''}'.trim();
+      final newMessage = Message.text(
+        id: oldMessage!.id,
+        authorId: oldMessage!.authorId,
+        text: response,
+        sentAt: DateTime.now(),
+      );
+      chatController.updateMessage(oldMessage!, newMessage);
+      oldMessage = newMessage;
+    });
+
+    // clear attachment
+    attachment.value = null;
+  }
+}
